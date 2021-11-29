0e04c09edc4ad7ec0bd857903e1385fc5d481c80
liuzenghui2007 <240164005@qq.com>
2021-11-29 20:48:37 +0800

    增加路由完成

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,13 +1,17 @@
 /*
  * @Author: 刘增辉
  * @Date: 2021-11-29 10:47:41
- * @LastEditTime: 2021-11-29 11:02:41
+ * @LastEditTime: 2021-11-29 20:47:23
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /cra0/src/routers/index.jsx
  */
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { 
+    BrowserRouter, 
+    Route, 
+    Routes 
+} from "react-router-dom";
 
 import Login from "../views/login/Login"
 import Home from "../views/home/Home"
@@ -16,8 +20,8 @@ const Routers = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={Login} />
-                <Route path="/home" element={Home} />
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
