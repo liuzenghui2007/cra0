e84fe92d3ba322a4409a3f031179dc61cd379719
liuzenghui2007 <240164005@qq.com>
2021-11-29 21:38:09 +0800

    添加路由懒加载与suspense fallback

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,11 +1,15 @@
 /*
  * @Author: 刘增辉
  * @Date: 2021-11-29 10:47:41
- * @LastEditTime: 2021-11-29 20:47:23
+ * @LastEditTime: 2021-11-29 21:36:29
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /cra0/src/routers/index.jsx
  */
+import { 
+    Suspense,
+    lazy
+} from 'react'
 
 import { 
     BrowserRouter, 
@@ -13,16 +17,18 @@ import {
     Routes 
 } from "react-router-dom";
 
-import Login from "../views/login/Login"
-import Home from "../views/home/Home"
+const Login = lazy(() => import("../views/login/Login")) 
+const Home = lazy(() => import("../views/home/Home"))
 
 const Routers = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-            </Routes>
+            <Suspense fallback={<div>Loading</div>}>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     )
 }
