c263758df9f6f0b41f7223ad797530658de34103
刘增辉 <240164005@qq.com>
2021-11-30 10:14:04 +0800

    引入antd

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,14 +1,21 @@
 /*
  * @Author: 刘增辉
  * @Date: 2021-11-28 20:40:57
- * @LastEditTime: 2021-11-28 20:48:00
+ * @LastEditTime: 2021-11-30 10:05:50
  * @LastEditors: Please set LastEditors
  * @Description: 首页页面
  * @FilePath: /my-react-app0/src/views/home/Home.jsx
  */
+
+import { Button, message } from "antd"
 const Home = () => {
     return (
-        <div>This is Home View</div>
+        <div>
+            <div>This is Home View</div>
+            <Button type="primary" style={{ marginLeft: 8 }}>显示messageg</Button>
+        </div>
+
+
     );
 }
 
--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,14 +1,22 @@
 /*
  * @Author: 刘增辉
  * @Date: 2021-11-28 20:43:16
- * @LastEditTime: 2021-11-28 20:48:08
+ * @LastEditTime: 2021-11-30 10:13:02
  * @LastEditors: Please set LastEditors
  * @Description: 用户登录页面
  * @FilePath: /my-react-app0/src/views/login/Login.jsx
  */
-
+import { Button, message } from "antd"
+const handleClick = (event) => {
+    console.log(event)
+    message.info(`${event.clientX}, ${event.clientY}`)
+}
 const Login = () => {
-    return (<div>This is Login view</div>);
+    return (
+    <div>
+        This is Login view
+        <Button type="primary" style={{ marginLeft: 8 }} onClick={handleClick}>弹出点击坐标</Button>
+    </div>);
 }
 
 export default Login;
