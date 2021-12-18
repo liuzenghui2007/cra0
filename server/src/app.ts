aadfadf7921634bd91211c5985b9c50017dce81c
liuzenghui2007 <240164005@qq.com>
2021-12-18 20:48:19 +0800

    增加server端接收数据的大小到100mb，但mongodb仍然保存不了

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-18 09:20:07
+ * @LastEditTime: 2021-12-18 20:45:50
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /cra0/server/src/app.ts
+ */
 import path from 'path';
 import favicon from 'serve-favicon';
 import compress from 'compression';
@@ -32,8 +40,8 @@ app.use(helmet({
 }));
 app.use(cors());
 app.use(compress());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', express.static(app.get('public')));
